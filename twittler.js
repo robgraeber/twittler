9c408aa737691ae2c1b346ceb44fe661de3e37ec
Rob Graeber <bgraeber@gmail.com>
2014-02-03 19:14:54 -0800

    3rd + 4th feature: Added CSS, refresh button, and user page

--- twittler.js
+++ twittler.js
@@ -1,20 +1,45 @@
-var refreshTweets = function(){
-	var $body = $('body');
-	$body.html('');
-	var index = streams.home.length - 1;
+var hashTag = function(){
+    var user = window.location.hash;
+    user = user.replace(/#/g,"");
+    return user;
+}
+var refreshTweets = function(postArray){
+    var $body = $("#tweetBox");
+    $body.html('');
+    var index = postArray.length - 1;
     while(index >= 0){
-      var tweet = streams.home[index];
-      var $tweet = $('<div></div>');
-      $tweet.text(tweet.created_at+": @" + tweet.user + ': ' + tweet.message);
+      var tweet = postArray[index];
+      var $tweet = $('<div class="tweet"></div>');
+      $tweet.append("<a href='#"+tweet.user+"'><b>@" + tweet.user + ":</b></a> " + tweet.message);
+      $tweet.append("<p>"+tweet.created_at+"</p>");
       $tweet.appendTo($body);
       index -= 1;
     }
+    $(".tweet a").click(function(){
+        setTimeout(function(){
+            refreshPage();
+        }, 0);
+    });
+};
+var refreshPage = function (){
+    var btn = $(".refreshBtn");
+    var postArray;
+    if(hashTag() === "" || hashTag() === undefined){
+        postArray = streams.home;
+    }else{
+        postArray = streams.users[hashTag()];
+       
+    }
+    refreshTweets(postArray);
 };
 $(document).ready(function(){
-       refreshTweets();
-       setInterval(refreshTweets, 1.0);
-	}
-);
+    refreshPage();
+    btn.click(function(){
+        setTimeout(function(){
+        refreshPage();
+        }, 0);
+    });
+});
 
 
 
